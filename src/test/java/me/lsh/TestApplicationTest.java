9f92ae14a19900bd95d8c8322e8f4604ffe35e78
iseunghun <lsh901673@gmail.com>
2023-04-25 15:35:33 +0900

    chore: init project

---
+++ src/test/java/me/lsh/TestApplicationTest.java
@@ -0,0 +1,16 @@
+package me.lsh;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TestApplicationTest {
+
+    @Test
+    @DisplayName("테스트")
+    void testApplication() {
+        assertEquals(3,1 + 2);
+    }
+
+}
