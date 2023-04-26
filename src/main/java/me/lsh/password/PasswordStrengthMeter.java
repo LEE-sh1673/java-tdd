aee658f3fda6096c1c0a36295aaa6f0961b591df
iseunghun <lsh901673@gmail.com>
2023-04-26 23:19:05 +0900

    feat: 암호 강도(보통)를 구하는 기능 추가

--- src/main/java/me/lsh/password/PasswordStrengthMeter.java
+++ src/main/java/me/lsh/password/PasswordStrengthMeter.java
@@ -3,6 +3,15 @@ package me.lsh.password;
 public class PasswordStrengthMeter {
 
     public PasswordStrength meter(final String password) {
+        if (password.length() < 8) {
+            return PasswordStrength.NORMAL;
+        }
+        if (!password.matches(".*[0-9].*")) {
+            return PasswordStrength.NORMAL;
+        }
+        if (!password.matches(".*[A-Z].*")) {
+            return PasswordStrength.NORMAL;
+        }
         return PasswordStrength.STRONG;
     }
 }
--- src/test/java/me/lsh/password/PasswordStrengthMeterTest.java
+++ src/test/java/me/lsh/password/PasswordStrengthMeterTest.java
@@ -6,10 +6,30 @@ import org.junit.jupiter.api.Test;
 
 class PasswordStrengthMeterTest {
 
+    private final PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+    void assertStrength(final String password, final PasswordStrength strength) {
+        assertEquals(strength, meter.meter(password));
+    }
+
     @Test
     void meetsAllCriteria_then_strong() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength strength = meter.meter("ab12!@AB");
-        assertEquals(PasswordStrength.STRONG, strength);
+        assertStrength("ab12!@AB", PasswordStrength.STRONG);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_length_then_normal() {
+        assertStrength("ab12!@A", PasswordStrength.NORMAL);
+        assertStrength("AB12!A%", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_number_then_normal() {
+        assertStrength("ab!@!@!@", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_uppercase_then_normal() {
+        assertStrength("ab12!@ab", PasswordStrength.NORMAL);
     }
 }
