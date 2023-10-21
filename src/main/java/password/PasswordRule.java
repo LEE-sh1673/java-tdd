96c201b056e009184860120a9bb11b25001a8f3a
LEE-sh1673 <lsh901673@gmail.com>
2023-10-21 20:20:14 +0900

    refactor: PasswordRule 타입 변경
    - interface -> enum
    
    다음과 같은 이유로 변경:
    - 구현체의 로직이 단순함
    - 구현 로직이 중복됨
    - 암호 규칙에 대한 캡슐화 필요
    - 인터페이스는 추후 확장시 고려해 봐도 무관

--- src/main/java/password/PasswordRule.java
+++ src/main/java/password/PasswordRule.java
@@ -1,6 +1,38 @@
 package password;
 
-public interface PasswordRule {
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
-    boolean check(final Password password);
+public enum PasswordRule {
+
+    IS_GREATER_OR_EQUAL_TO_EIGHT(password -> password.getLength() == 8),
+
+    CONTAINS_UPPER_CASE(PasswordPattern.UPPER_CASE::matches),
+
+    CONTAINS_DIGIT(PasswordPattern.DIGIT::matches);
+
+    private Predicate<Password> rule;
+
+    PasswordRule(final Predicate<Password> rule) {
+        this.rule = rule;
+    }
+
+    public boolean check(final Password password) {
+        return rule.test(password);
+    }
+
+    private enum PasswordPattern {
+        UPPER_CASE(".*[A-Z].*"),
+        DIGIT(".*\\d.*");
+
+        private final String regex;
+
+        PasswordPattern(final String regex) {
+            this.regex = regex;
+        }
+
+        public boolean matches(final Password password) {
+            return password.matches(Pattern.compile(regex));
+        }
+    }
 }
--- src/test/java/password/PasswordRuleCheckerTest.java
+++ src/test/java/password/PasswordRuleCheckerTest.java
@@ -10,7 +10,7 @@ public class PasswordRuleCheckerTest {
     @Test
     @DisplayName("길이가 8글자 이상인지 검사할 수 있다.")
     void isLengthGreaterThanOrEqualsToEight_Then_True() {
-        final PasswordRule rule = new PasswordLengthRule();
+        final PasswordRule rule = PasswordRule.IS_GREATER_OR_EQUAL_TO_EIGHT;
         assertThat(rule.check(Password.of("12345678"))).isTrue();
         assertThat(rule.check(Password.of("1234567"))).isFalse();
     }
@@ -18,7 +18,7 @@ public class PasswordRuleCheckerTest {
     @Test
     @DisplayName("0부터 9사이의 숫자를 포함하는지 검사할 수 있다")
     void containsZeroToNineNumber_Then_True() {
-        final PasswordRule rule = new PasswordNumberRule();
+        final PasswordRule rule = PasswordRule.CONTAINS_DIGIT;
         assertThat(rule.check(Password.of("0123456789"))).isTrue();
         assertThat(rule.check(Password.of("abcdefg"))).isFalse();
         assertThat(rule.check(Password.of("2abc0defg"))).isTrue();
@@ -27,7 +27,7 @@ public class PasswordRuleCheckerTest {
     @Test
     @DisplayName("대문자를 포함하는지 검사할 수 있다.")
     void containsUpperCase_Then_True() {
-        final PasswordRule rule = new PasswordUpperCaseRule();
+        final PasswordRule rule = PasswordRule.CONTAINS_UPPER_CASE;
         assertThat(rule.check(Password.of("AAAA"))).isTrue();
         assertThat(rule.check(Password.of("0123456789"))).isFalse();
         assertThat(rule.check(Password.of("2abc0defg"))).isFalse();
